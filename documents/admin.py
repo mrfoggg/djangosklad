f3d9b6307c390834ff7dd2484e9e5f14701a4db8
mrfoggg <mrfoggg@gmail.com>
2026-04-30 15:44:41 +0300

    реализвали установку цен поставщиков

--- documents/admin.py
+++ documents/admin.py
@@ -1,3 +1,45 @@
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
+from unfold.admin import ModelAdmin, TabularInline
 
-# Register your models here.
+from .models import SupplierPriceItem, SupplierPriceList
+
+
+class SupplierPriceItemInline(TabularInline):
+    model = SupplierPriceItem
+    extra = 1
+    # Здесь можно будет добавить отображение цены в UAH,
+    # если у поставщика включен признак use_usd_prices.
+    fields = ("product", "price")
+
+
+@admin.register(SupplierPriceList)
+class SupplierPriceListAdmin(ModelAdmin):
+    list_display = ("id", "supplier", "dt_created", "is_applied", "to_remove")
+    list_filter = ("is_applied", "to_remove", "supplier")
+    search_fields = ("id", "supplier__last_name")
+
+    inlines = [SupplierPriceItemInline]
+
+    fieldsets = (
+        (
+            None,
+            {
+                "fields": (
+                    "id",
+                    "supplier",
+                    "is_applied",
+                    "comment",
+                )
+            },
+        ),
+        (
+            _("Служебная информация"),
+            {
+                "fields": ("dt_created", "dt_updated", "dt_applied", "to_remove"),
+                "classes": ("collapse",),
+            },
+        ),
+    )
+
+    readonly_fields = ("id", "dt_created", "dt_updated", "dt_applied")
--- documents/models.py
+++ documents/models.py
@@ -1,49 +1,83 @@
+from decimal import Decimal
+
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+from djmoney.models.fields import MoneyField
+
 
 class BaseDocumentModel(models.Model):
     """
     Абстрактная база для всех документов.
     """
-    number = models.CharField(
-        max_length=50,
-        verbose_name=_('Номер'),
-        db_index=True
-    )
+
     dt_created = models.DateTimeField(
-        verbose_name=_('Создан'),
-        auto_now_add=True,
-        db_index=True
+        verbose_name=_("Создан"), auto_now_add=True, db_index=True
     )
-    dt_updated = models.DateTimeField(
-        verbose_name=_('Изменен'),
-        auto_now=True
-    )
+    dt_updated = models.DateTimeField(verbose_name=_("Изменен"), auto_now=True)
 
     # Флаг применения документа к учету
     is_applied = models.BooleanField(
-        verbose_name=_('Применен'),
+        verbose_name=_("Применен"),
         default=False,
         db_index=True,
-        help_text=_("Документ проведен и учитывается в остатках")
+        help_text=_("Документ проведен и учитывается в остатках"),
     )
     dt_applied = models.DateTimeField(
-        verbose_name=_('Дата применения'),
-        null=True,
-        blank=True,
-        editable=False
+        verbose_name=_("Дата применения"), null=True, blank=True, editable=False
     )
 
     to_remove = models.BooleanField(
-        verbose_name=_('В корзине'),
-        default=False,
-        db_index=True
+        verbose_name=_("В корзине"), default=False, db_index=True
     )
 
     class Meta:
         abstract = True
-        ordering = ['-dt_created']
+        ordering = ["-dt_created"]
 
     def __str__(self):
         status = _("Применен") if self.is_applied else _("Черновик")
-        return f"{self._meta.verbose_name} №{self.number} ({status})"
+        return f"{self._meta.verbose_name} №{self.id} ({status})"
+
+
+# Набор доступных валют
+AVAILABLE_CURRENCIES = [("UAH", "UAH"), ("USD", "USD")]
+
+
+class SupplierPriceList(BaseDocumentModel):
+    supplier = models.ForeignKey(
+        "catalogs.Contractor",  # Ссылка на Contractor в catalogs
+        on_delete=models.CASCADE,
+        limit_choices_to={"is_supplier": True},
+        verbose_name=_("Поставщик"),
+        related_name="price_lists",
+    )
+    comment = models.TextField(_("Комментарий"), blank=True)
+
+    class Meta(BaseDocumentModel.Meta):
+        verbose_name = _("Установка цен поставщика")
+        verbose_name_plural = _("Установки цен поставщика")
+
+
+class SupplierPriceItem(models.Model):
+    document = models.ForeignKey(
+        "SupplierPriceList",
+        on_delete=models.CASCADE,
+        related_name="items",
+        verbose_name=_("Документ"),
+    )
+    product = models.ForeignKey(
+        "catalogs.Product",  # Исправлено: теперь тоже из catalogs
+        on_delete=models.PROTECT,
+        verbose_name=_("Товар"),
+    )
+    price = MoneyField(
+        max_digits=12,
+        decimal_places=2,
+        default_currency="UAH",
+        currency_choices=AVAILABLE_CURRENCIES,
+        verbose_name=_("Цена"),
+    )
+
+    class Meta:
+        verbose_name = _("Позиция прайса")
+        verbose_name_plural = _("Позиции прайса")
